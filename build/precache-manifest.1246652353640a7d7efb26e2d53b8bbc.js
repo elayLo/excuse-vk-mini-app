24ff37521d8328e3c2be7aeea1ce54a7c8db5a22
a.sergeenkov <a.sergeenkov@int.robotbull.com>
2020-05-03 16:21:01 +0300

    Initial

---
+++ build/precache-manifest.1246652353640a7d7efb26e2d53b8bbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fecbceac91dbaa6632784c42d46eff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "393534921d1491632995",
+    "url": "./static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "f1cd709ede9b67f737ce",
+    "url": "./static/css/main.c79009f4.chunk.css"
+  },
+  {
+    "revision": "393534921d1491632995",
+    "url": "./static/js/2.1c4d9067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1c4d9067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cd709ede9b67f737ce",
+    "url": "./static/js/main.5320c46f.chunk.js"
+  },
+  {
+    "revision": "02eb7e5f24a8eb60c357",
+    "url": "./static/js/runtime-main.5d409247.js"
+  },
+  {
+    "revision": "efc0cfd68b8292c0de7d2e80a64a7d8c",
+    "url": "./static/media/multimedia-option.efc0cfd6.svg"
+  }
+]);
